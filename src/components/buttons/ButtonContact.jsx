1c03153231c1da72ee96085a76649377438afdaa
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-04-02 15:12:00 -0300

    add dist

--- src/components/buttons/ButtonContact.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../../sass/components/buttonContact.sass";
-
-const ButtonContact = ({subClass, textButton}) => {
-  return <button className={subClass}>{textButton}</button>;
-};
-
-export default ButtonContact;
